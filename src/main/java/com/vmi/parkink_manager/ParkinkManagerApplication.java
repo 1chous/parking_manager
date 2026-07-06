8c2d172481e66c9e1088c0b43c908c84c6e7173b
Владимир Серебряков <vladimir9802@yandex.ru>
2026-07-06 11:24:23 +0300

    add project template

---
+++ src/main/java/com/vmi/parkink_manager/ParkinkManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vmi.parkink_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinkManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinkManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmi/parkink_manager/ParkinkManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmi.parkink_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinkManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
